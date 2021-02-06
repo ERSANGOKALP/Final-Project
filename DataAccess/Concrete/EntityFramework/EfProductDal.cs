d85f7c6e220c0a1ced3a627cdf0c33016b3ddef0
ERSANGOKALP <71315637+ERSANGOKALP@users.noreply.github.com>
2021-02-06 13:16:10 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,101 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        void Add(Product entity)
+        {
+            using (NortWindContext context=new NortWindContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        void IEntityRepository<Product>.Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Delete(Product entity)
+        {
+            using (NortWindContext context = new NortWindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        void IEntityRepository<Product>.Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        Product IEntityRepository<Product>.Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Product> IEntityRepository<Product>.GetAll(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEntityRepository<Product>.Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+        Product Get(Expression<Func<Product, bool>> filter)
+        {
+        using (NortWindContext context= new NortWindContext())
+        {
+            return context.Set<Product>().SingleOrDefault(filter);
+        }
+        }
+
+        List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+        using (NortWindContext context=new NortWindContext())
+        {
+            return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+        }
+        }
+
+        void Update(Product entity)
+        {
+        using (NortWindContext context = new NortWindContext())
+        {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+    
+}
---
+++ DataAccess/Concrete/EntityFramework/NortWindContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NortWindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localDB\mssqlLocalDb(sqlserver 13.0.4001.0-casper\administrator");
+
+        }
+    }
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Product> Categories { get; set; }
+
+    public DbSet<Product> Customers { get; set; }
+
+
+}
